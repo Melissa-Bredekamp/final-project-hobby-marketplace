a6e8d227a4504a30f361809bd9c59308038c19d2
melissabredekamp <mbredekamp@gmail.com>
2020-11-25 23:28:01 +0100

    Fix [id].js else if statemenFix [id].js else if statementt

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -4,7 +4,7 @@ import {
   deleteUserById,
   updateUserById,
 } from '../../../utilities/database';
-import {photoUpload} from '../../../utilities/storage';
+import { photoUpload } from '../../../utilities/storage';
 
 var cloudinary = require('cloudinary').v2;
 cloudinary.config({
@@ -12,7 +12,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
-
 
 export default async function handler(request, response) {
   const userId = request.query.id;
@@ -29,30 +28,28 @@ export default async function handler(request, response) {
     user = await getUserById(userId);
   } else if (request.method === 'PATCH') {
     const newUser = request.body.user;
-    
+
     // console.log(request.body.user.photo);
 
     if (newUser.photoFile) {
       // 1. request.....photo -> photoFile. .photo ist ab jetzt immer die URL. .photoFile = neues Bild im edit user request only.
       // 2. neue Funktion schreiben: photoFile übergeben und photoUrl erhalten.
       // 3.
-        newUser.photo = (await photoUpload(newUser.photoFile)) || '';
-         delete newUser.photoFile;
+      newUser.photo = (await photoUpload(newUser.photoFile)) || '';
+      delete newUser.photoFile;
       const data = request.body.user.photo;
 
-          // console.log('x1 updateUserById', userId, {
-          //   photo: result.secure_url,
-          // });
-          const user = await updateUserById(userId, {
-            photo: result.secure_url,
-          });
-          // console.log('x2 updateUserById', userId, user);
-          // return response.status(200).send({ success: true, user: user });
-        }
-    } 
-    else
+      // console.log('x1 updateUserById', userId, {
+      //   photo: result.secure_url,
+      // });
+      const user = await updateUserById(userId, {
+        photo: result.secure_url,
+      });
+      // console.log('x2 updateUserById', userId, user);
+      // return response.status(200).send({ success: true, user: user });
+    }
     user = await updateUserById(parseInt(userId), newUser);
-  else if (request.method === 'DELETE') {
+  } else if (request.method === 'DELETE') {
     user = await deleteUserById(userId);
   }
 
